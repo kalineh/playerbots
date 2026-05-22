0ef337cbf48de9c9485fdc8d237026c3fa8f8784
kalin <kalin.eh@gmail.com>
2026-05-22 11:55:35 -0700

    Clear stale friend resupply travel

--- playerbot/FriendBotController.cpp
+++ playerbot/FriendBotController.cpp
@@ -26,7 +26,7 @@ using namespace ai;
 
 namespace
 {
-    const char* FRIEND_BOT_VERSION = "v19";
+    const char* FRIEND_BOT_VERSION = "v20";
     const uint8 FRIEND_MANA_BUFF_COMFORT = 75;
     const uint8 FRIEND_MANA_DAMAGE_CONSERVE = 85;
     const float FRIEND_RECOVER_HOSTILE_DISTANCE = 22.0f;
@@ -3623,7 +3623,8 @@ bool FriendBotController::TryTravelForResupply(const FriendSituation& situation)
         }
         else
         {
-            SetResult(lastIntent, "shop blocked:travel busy", FriendExecutionResult::BlockedNotUseful);
+            ClearFriendTravelTarget();
+            SetResult(lastIntent, "clear stale travel target", FriendExecutionResult::Done);
             ai->SetActionDuration(sPlayerbotAIConfig.globalCoolDown);
             return true;
         }
